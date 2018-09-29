3aacf2e52cedfd5ee7b485112b9f8fae71c1b813
unknown <ty-sany@hotmail.com>
2018-09-29 11:07:34 +0800

    Gl 添加bob优化项目说明!

--- ch07-gl-basics/app/src/main/java/com/badlogic/androidgames/glbasics/OptimizedBobTest.java
+++ ch07-gl-basics/app/src/main/java/com/badlogic/androidgames/glbasics/OptimizedBobTest.java
@@ -10,13 +10,20 @@ import com.badlogic.androidgames.framework.gl.Texture;
 import com.badlogic.androidgames.framework.impl.GLGame;
 import com.badlogic.androidgames.framework.impl.GLGraphics;
 
+
+/**
+ * 相比于bobTest产生的优化项目
+ *
+ *  开启了顶点GL_VERTEX_ARRAY ，用数组的方式减少调用Gl函数一次处理的顶点数量变多了。
+ *
+ */
 public class OptimizedBobTest extends GLGame {
 
     @Override
     public Screen getStartScreen() {
         return new OptimizedBobScreen(this);
     }
-    
+
     class OptimizedBobScreen extends Screen {
         static final int NUM_BOBS = 100;
         GLGraphics glGraphics;
@@ -24,35 +31,35 @@ public class OptimizedBobTest extends GLGame {
         BindableVertices bobModel;
         Bob[] bobs;
         FPSCounter fpsCounter;
-        
+
         public OptimizedBobScreen(Game game) {
             super(game);
-            glGraphics = ((GLGame)game).getGLGraphics();
-            
-            bobTexture = new Texture((GLGame)game, "bobrgb888-32x32.png");
-            
-            bobModel = new BindableVertices(glGraphics, 4, 12, false, true);
-            bobModel.setVertices(new float[] { -16, -16, 0, 1,  
-                                                16, -16, 1, 1, 
-                                                16,  16, 1, 0,
-                                               -16,  16, 0, 0, }, 0, 16);
-            bobModel.setIndices(new short[] {0, 1, 2, 2, 3, 0}, 0, 6);
+            glGraphics = ((GLGame) game).getGLGraphics();
 
-            
+            bobTexture = new Texture((GLGame) game, "bobrgb888-32x32.png");
+
+            bobModel = new BindableVertices(glGraphics, 4, 12, false, true);
+            bobModel.setVertices(new float[]{-16, -16, 0, 1,
+                    16, -16, 1, 1,
+                    16, 16, 1, 0,
+                    -16, 16, 0, 0,}, 0, 16);
+            bobModel.setIndices(new short[]{0, 1, 2, 2, 3, 0}, 0, 6);
+
+
             bobs = new Bob[100];
-            for(int i = 0; i < 100; i++) {
+            for (int i = 0; i < 100; i++) {
                 bobs[i] = new Bob();
             }
-            
+
             fpsCounter = new FPSCounter();
         }
-        
+
         @Override
-        public void update(float deltaTime) {         
+        public void update(float deltaTime) {
             game.getInput().getTouchEvents();
             game.getInput().getKeyEvents();
-            
-            for(int i = 0; i < NUM_BOBS; i++) {
+
+            for (int i = 0; i < NUM_BOBS; i++) {
                 bobs[i].update(deltaTime);
             }
         }
@@ -65,40 +72,40 @@ public class OptimizedBobTest extends GLGame {
             gl.glMatrixMode(GL10.GL_PROJECTION);
             gl.glLoadIdentity();
             gl.glOrthof(0, 320, 0, 480, 1, -1);
-            
+
             gl.glMatrixMode(GL10.GL_MODELVIEW);
-            
-            bobTexture.reload();            
+
+            bobTexture.reload();
             gl.glEnable(GL10.GL_TEXTURE_2D);
             bobTexture.bind();
         }
-        
+
         @Override
         public void present(float deltaTime) {
-            GL10 gl = glGraphics.getGL();            
-            gl.glClear(GL10.GL_COLOR_BUFFER_BIT);                        
-                        
+            GL10 gl = glGraphics.getGL();
+            gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
+
             bobModel.bind();
-            for(int i = 0; i < NUM_BOBS; i++) {
+            for (int i = 0; i < NUM_BOBS; i++) {
                 gl.glLoadIdentity();
-                gl.glTranslatef((int)bobs[i].x, (int)bobs[i].y, 0);
+                gl.glTranslatef((int) bobs[i].x, (int) bobs[i].y, 0);
                 bobModel.draw(GL10.GL_TRIANGLES, 0, 6);
             }
             bobModel.unbind();
-            
+
             fpsCounter.logFrame();
         }
 
         @Override
         public void pause() {
             // TODO Auto-generated method stub
-            
-        }       
+
+        }
 
         @Override
         public void dispose() {
             // TODO Auto-generated method stub
-            
+
         }
     }
 }
